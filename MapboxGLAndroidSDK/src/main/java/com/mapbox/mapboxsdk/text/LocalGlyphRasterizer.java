486c4f4b4aa42ec7a82f55e8752750200872c3e8
zmiao <miao.zhao@mapbox.com>
2020-07-24 14:56:37 +0300

    update

--- MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/text/LocalGlyphRasterizer.java
+++ MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/text/LocalGlyphRasterizer.java
@@ -26,34 +26,22 @@ public class LocalGlyphRasterizer {
     public int descender;
     public int advance;
     public Bitmap glyphBitmap;
-
-    GlyphMetrics() {
-      this.glyphBitmap = Bitmap.createBitmap(35, 35, Bitmap.Config.ARGB_8888);
-      this.width = 0;
-      this.height = 0;
-      this.top = 0;
-      this.left = 0;
-      this.ascender = 0;
-      this.descender = 0;
-      this.advance = 0;
-    }
   }
+  @NonNull
   private final GlyphMetrics glyphMetrics;
-
-  private final Bitmap bitmap;
+  @NonNull
+  private final Rect bounds;
   @NonNull
   private final Paint paint;
   @NonNull
   private final Canvas canvas;
 
   LocalGlyphRasterizer() {
-    /*
-      35x35px dimensions are hardwired to match local_glyph_rasterizer.cpp
-      These dimensions are large enough to draw a 24 point character in the middle
-      of the bitmap (y: 20) with some buffer around the edge
-    */
-    bitmap = Bitmap.createBitmap(35, 35, Bitmap.Config.ARGB_8888);
-    glyphMetrics = this.new GlyphMetrics();
+    glyphMetrics = new GlyphMetrics();
+    glyphMetrics.glyphBitmap = Bitmap.createBitmap(35, 35, Bitmap.Config.ARGB_8888);
+
+    bounds = new Rect();
+
     paint = new Paint();
     paint.setAntiAlias(true);
     paint.setTextSize(24);
@@ -63,44 +51,38 @@ public class LocalGlyphRasterizer {
   }
 
   /***
-   * Uses Android-native drawing code to rasterize a single glyph
-   * to a square {@link Bitmap} which can be returned to portable
-   * code for transformation into a Signed Distance Field glyph.
+   * Uses Android-native drawing code to encapsulate the glyph metrics and a raterized
+   * square {@link Bitmap} to a single structure which can be returned to portable
+   * code for transformation into the native glyph data structure.
    *
    * @param fontFamily Font family string to pass to Typeface.create
    * @param bold If true, use Typeface.BOLD option
    * @param glyphID 16-bit Unicode BMP codepoint to draw
    *
-   * @return Return a {@link Bitmap} to be displayed in the requested tile.
+   * @return Return GlyphMetrics that contains all the necessary information to display the character in the requested tile.
    */
-  @WorkerThread
-  protected Bitmap drawGlyphBitmap(String fontFamily, boolean bold, char glyphID) {
-    paint.setTypeface(Typeface.create(fontFamily, bold ? Typeface.BOLD : Typeface.NORMAL));
-    canvas.drawColor(Color.WHITE);
-    canvas.drawText(String.valueOf(glyphID), 5, 25, paint);
-    return glyphMetrics.glyphBitmap;
-  }
-
   @WorkerThread
   protected GlyphMetrics getGlyphMetrics(String fontFamily, boolean bold, char glyphID) {
     paint.setTypeface(Typeface.create(fontFamily, bold ? Typeface.BOLD : Typeface.NORMAL));
-    canvas.drawColor(Color.WHITE);
-    canvas.drawText(String.valueOf(glyphID), 5, 25, paint);
 
+    // Get font metrics
     Paint.FontMetricsInt metrics = paint.getFontMetricsInt();
-    glyphMetrics.ascender = Math.abs(metrics.ascent);
-    glyphMetrics.descender = Math.abs(metrics.descent);
+    glyphMetrics.ascender = Math.abs(metrics.top);
+    glyphMetrics.descender = Math.abs(metrics.bottom);
 
-    Rect bounds = new Rect();
-    paint.getTextBounds(String.valueOf(glyphID), 0, 1, bounds);
+    String glyph = String.valueOf(glyphID);
+    paint.getTextBounds(glyph, 0, 1, bounds);
     glyphMetrics.left = bounds.left;
     glyphMetrics.width = bounds.right - bounds.left;
     glyphMetrics.height = bounds.bottom - bounds.top;
-    glyphMetrics.top = Math.abs(bounds.top) - glyphMetrics.ascender;
-    float width = paint.measureText(String.valueOf(glyphID), 0, 1);
-    glyphMetrics.advance = Math.round(width);
-//    glyphMetrics.glyphBitmap = bitmap;
-//    Bitmap emptyBitmap = Bitmap.createBitmap(35, 35, Bitmap.Config.ARGB_8888);
+    glyphMetrics.top = Math.abs(bounds.top);
+    glyphMetrics.advance = Math.round(paint.measureText(glyph, 0, 1));
+
+    // Draw glyph on the canvas with the 3 pixels' boarder padding from the left and top
+    float x = 3 - bounds.left;
+    float y = 3 - bounds.top;
+    canvas.drawColor(Color.WHITE);
+    canvas.drawText(glyph, x, y, paint);
 
     return glyphMetrics;
   }
